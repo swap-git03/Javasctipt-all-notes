d13f8b66c3c744f0e65915956827ee3900693167
swap-git03 <swapnilfulpagare061003@gmail.com>
2025-08-12 22:42:11 +0530

    done!

--- interview.js
+++ interview.js
@@ -171,4 +171,317 @@ console.log(isPrime(15)); // false
 }
 
 
- 4. Check if a string is a palindrome
+
+ ## 4. Check if a string is a palindrome
+function isPalindrome(str) {
+  let reversed = str.split('').reverse().join('');
+  return str === reversed;
+}
+
+console.log(isPalindrome("madam")); // true
+console.log(isPalindrome("hello")); // false
+## 5. Generate the Fibonacci sequence up to n terms
+function fibonacci(n) {
+  let sequence = [0, 1];
+  while (sequence.length < n) {
+    sequence.push(sequence[sequence.length - 1] + sequence[sequence.length - 2]);
+  }
+  return sequence;
+}
+
+console.log(fibonacci(10)); // [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+## 6. Find the second largest number in an array
+function secondLargest(arr) {
+  let max = Math.max(...arr);
+  arr = arr.filter(num => num !== max);
+  return Math.max(...arr);
+}
+
+console.log(secondLargest([1, 2, 3, 4, 5])); // 4
+## 7. Count the number of vowels in a string
+function countVowels(str) {
+  let vowels = 'aeiou';
+  let count = 0;
+  for (let char of str.toLowerCase()) {
+    if (vowels.includes(char)) count++;
+  }
+  return count;
+}
+
+console.log(countVowels("hello world")); // 3
+## 8. Remove all duplicates from an array
+function removeDuplicates(arr) {
+  return [...new Set(arr)];
+}
+
+console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5])); // [1, 2, 3, 4, 5]
+## 9. Sort an array without using the built-in sort() method
+function sortArray(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] > arr[j]) {
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(sortArray([5, 2, 8, 1, 9])); // [1, 2, 5, 8, 9]
+## 10. Find the sum of all numbers in an array that are divisible by 3 or 5
+function sumDivisibleBy3Or5(arr) {
+  return arr.reduce((sum, num) => num % 3 === 0 || num % 5 === 0 ? sum + num : sum, 0);
+}
+
+console.log(sumDivisibleBy3Or5([1, 2, 3, 4, 5, 6])); // 3 + 5 + 6 = 14
+## 11. Check whether a given year is a leap year or not
+function isLeapYear(year) {
+  return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
+}
+
+console.log(isLeapYear(2020)); // true
+console.log(isLeapYear(2021)); // false
+## 12. Count the number of words in a sentence
+function countWords(sentence) {
+  return sentence.trim().split(/\s+/).length;
+}
+
+console.log(countWords("hello world")); // 2
+
+
+
+## 13. Find the GCD (Greatest Common Divisor) of two numbers
+function gcd(a, b) {
+  if (b === 0) return a;
+  return gcd(b, a % b);
+}
+
+console.log(gcd(48, 18)); // 6
+## 14. Find the longest word in a given string
+function longestWord(str) {
+  let words = str.split(' ');
+  let longest = words[0];
+  for (let word of words) {
+    if (word.length > longest.length) longest = word;
+  }
+  return longest;
+}
+
+console.log(longestWord("I love programming")); // programming
+## 15. Convert the first letter of each word in a sentence to uppercase
+function capitalizeFirstLetter(str) {
+  return str.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+}
+
+console.log(capitalizeFirstLetter("hello world")); // Hello World
+## 16. Flatten a nested array
+function flattenArray(arr) {
+  return arr.reduce((flat, toFlatten) => {
+    return flat.concat(Array.isArray(toFlatten) ? flattenArray(toFlatten) : toFlatten);
+  }, []);
+}
+
+console.log(flattenArray([1, [2, [3, 4], 5]])); // [1, 2, 3, 4, 5]
+## 17. Check if two strings are anagrams
+function areAnagrams(str1, str2) {
+  return str1.split('').sort().join('') === str2.split('').sort().join('');
+}
+
+console.log(areAnagrams("The Morse code is anagrams of Morse code")); // true
+console.log(areAnagrams("hello", "world")); // false
+## 18. Find the number of occurrences of a specific element in an array
+function countOccurrences(arr, element) {
+  return arr.filter(item => item === element).length;
+}
+
+console.log(countOccurrences([1, 2, 2, 3, 2], 2)); // 3
+## 19. Shuffle the elements of an array (Fisher-Yates Shuffle)
+function shuffleArray(arr) {
+  for (let i = arr.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr;
+}
+
+console.log(shuffleArray([1, 2, 3, 4, 5]));
+## 20. Remove all falsy values from an array
+function removeFalsyValues(arr) {
+  return arr.filter(Boolean);
+}
+
+console.log(removeFalsyValues([0, null, undefined, '', 'hello', true])); // ['hello', true]
+
+
+
+Here are the solutions to the given problems:
+
+## 1. Check if a number is prime or not
+function isPrime(num) {
+  if (num <= 1) return false;
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+}
+
+console.log(isPrime(11)); // true
+console.log(isPrime(15)); // false
+## 2. Reverse a string without using built-in reverse methods
+function reverseString(str) {
+  let reversed = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+  return reversed;
+}
+
+console.log(reverseString("hello")); // olleh
+## 3. Find the factorial of a number using recursion
+function factorial(n) {
+  if (n === 0 || n === 1) return 1;
+  return n * factorial(n - 1);
+}
+
+console.log(factorial(5)); // 120
+## 4. Check if a string is a palindrome
+function isPalindrome(str) {
+  let left = 0;
+  let right = str.length - 1;
+  while (left < right) {
+    if (str[left] !== str[right]) return false;
+    left++;
+    right--;
+  }
+  return true;
+}
+
+console.log(isPalindrome("madam")); // true
+console.log(isPalindrome("hello")); // false
+## 5. Generate the Fibonacci sequence up to n terms
+function fibonacci(n) {
+  let sequence = [0, 1];
+  while (sequence.length < n) {
+    sequence.push(sequence[sequence.length - 1] + sequence[sequence.length - 2]);
+  }
+  return sequence;
+}
+
+console.log(fibonacci(10)); // [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+## 6. Find the second largest number in an array
+function secondLargest(arr) {
+  let max = Math.max(...arr);
+  arr = arr.filter(num => num !== max);
+  return Math.max(...arr);
+}
+
+console.log(secondLargest([1, 2, 3, 4, 5])); // 4
+## 7. Count the number of vowels in a string
+function countVowels(str) {
+  let vowels = 'aeiou';
+  let count = 0;
+  for (let char of str.toLowerCase()) {
+    if (vowels.includes(char)) count++;
+  }
+  return count;
+}
+
+console.log(countVowels("hello world")); // 3
+## 8. Remove all duplicates from an array
+function removeDuplicates(arr) {
+  return [...new Set(arr)];
+}
+
+console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5])); // [1, 2, 3, 4, 5]
+## 9. Sort an array without using the built-in sort() method
+function sortArray(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] > arr[j]) {
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(sortArray([5, 2, 8, 1, 9])); // [1, 2, 5, 8, 9]
+## 10. Find the sum of all numbers in an array that are divisible by 3 or 5
+function sumDivisibleBy3Or5(arr) {
+  return arr.reduce((sum, num) => (num % 3 === 0 || num % 5 === 0) ? sum + num : sum, 0);
+}
+
+console.log(sumDivisibleBy3Or5([1, 2, 3, 4, 5, 6])); // 3 + 5 + 6 = 14
+## 11. Check whether a given year is a leap year or not
+function isLeapYear(year) {
+  return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+}
+
+console.log(isLeapYear(2020)); // true
+console.log(isLeapYear(2021)); // false
+
+## 12. Count the number of words in a sentence
+function countWords(sentence) {
+  return sentence.trim().split(/\s+/).length;
+}
+
+console.log(countWords("hello world")); // 2
+## 13. Find the GCD (Greatest Common Divisor) of two numbers
+function gcd(a, b) {
+  if (b === 0) return a;
+  return gcd(b, a % b);
+}
+
+console.log(gcd(48, 18)); // 6
+## 14. Find the longest word in a given string
+function longestWord(str) {
+  let words = str.split(' ');
+  let longest = '';
+  for (let word of words) {
+    if (word.length > longest.length) longest = word;
+  }
+  return longest;
+}
+
+console.log(longestWord("I love programming")); // programming
+## 15. Convert the first letter of each word in a sentence to uppercase
+function capitalizeFirstLetter(str) {
+  return str.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+}
+
+console.log(capitalizeFirstLetter("hello world")); // Hello World
+## 16. Flatten a nested array
+function flattenArray(arr) {
+  return arr.flat(Infinity);
+}
+
+console.log(flattenArray([1, [2, [3, 4], 5]])); // [1, 2, 3, 4, 5]
+## 17. Check if two strings are anagrams
+function areAnagrams(str1, str2) {
+  return str1.split('').sort().join('') === str2.split('').sort().join('');
+}
+
+console.log(areAnagrams("listen", "silent")); // true
+console.log(areAnagrams("hello", "world")); // false
+## 18. Find the number of occurrences of a specific element in an array
+function countOccurrences(arr, element) {
+  return arr.filter(item => item === element).length;
+}
+
+console.log(countOccurrences([1, 2, 2, 3, 2], 2)); // 3
+## 19. Shuffle the elements of an array (Fisher-Yates Shuffle)
+function shuffleArray(arr) {
+  for (let i = arr.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr;
+}
+
+console.log(shuffleArray([1, 2, 3, 4, 5]));
+## 20. Remove all falsy values from an array
+function removeFalsyValues(arr) {
+  return arr.filter(Boolean);
+}
+
+console.log(removeFalsyValues([0, null, undefined, '', 'hello', true])); // ['hello', true]

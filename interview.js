dfca5dfcbc68f0dbf761f746dbfb84a940aa0dd7
swap-git03 <swapnilfulpagare061003@gmail.com>
2026-01-11 22:37:46 +0530

    /

--- interview.js
+++ interview.js
@@ -459,8 +459,9 @@
 // function flattenArray(arr) {
 //   return arr.flat(Infinity);
 // }
-
 // console.log(flattenArray([1, [2, [3, 4], 5]])); // [1, 2, 3, 4, 5]
+
+
 // ## 17. Check if two strings are anagrams
 // function areAnagrams(str1, str2) {
 //   return str1.split('').sort().join('') === str2.split('').sort().join('');

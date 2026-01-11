8a4555d16ed2e2a9b4ddb2d09fe687fcaac9b296
swap-git03 <swapnilfulpagare061003@gmail.com>
2026-01-11 22:37:18 +0530

    /

--- interview.js
+++ interview.js
@@ -465,7 +465,6 @@
 // function areAnagrams(str1, str2) {
 //   return str1.split('').sort().join('') === str2.split('').sort().join('');
 // }
-
 // console.log(areAnagrams("listen", "silent")); // true
 // console.log(areAnagrams("hello", "world")); // false
 // ## 18. Find the number of occurrences of a specific element in an array
